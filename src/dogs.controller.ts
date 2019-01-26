8126853a7631c2093ecf0b9dfa9e92d2ebcd3008
Seth <sethalexander@pm.me>
2019-01-25 22:35:07 -0600

    Simple crud controller

--- src/dogs.controller.ts
+++ src/dogs.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { identity } from 'rxjs';
 
 @Controller('dogs')
 export class DogsController {
@@ -7,8 +17,23 @@ export class DogsController {
     return 'This action adds a new dog';
   }
 
+  @Get()
+  findAll(@Query() query) {
+    return `This action returns all dogs (limit: ${query.limit}) items`;
+  }
+
   @Get(':id')
   findOne(@Param('id') id) {
     return `This action returns a #${id} dog`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id, @Body() updateDogDto) {
+    return `This actions updates a #${id} dog`;
+  }
+
+  @Delete('id')
+  remove(@Param('id') id) {
+    return `This action remove a #${id} dog`;
   }
 }
