566a5634ebf51415da01202700988d097f0395d5
Seth <sethalexander@pm.me>
2019-01-25 22:40:27 -0600

    Wire up dogs controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [DogsController],
 })
 export class AppModule {}
