224587803804c1a8bc08c067b143ada2b4a1a91d
Seth <sethalexander@pm.me>
2019-01-25 22:15:25 -0600

    Add post endpoint

--- src/dogs.controller.ts
+++ src/dogs.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('dogs')
 export class DogsController {
+  @Post()
+  create() {
+    return 'This action adds a new dog';
+  }
+
   @Get()
   findAll() {
     return 'This action returns all dogs';
